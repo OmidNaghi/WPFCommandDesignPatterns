8644127aa90dc48df5eb0e99f7d20883b8a468b0
OmidNaghi <43643964+OmidNaghi@users.noreply.github.com>
2018-10-01 13:49:16 +0200

    Refactoring

---
+++ WPFCommandDesignPattern/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCommandDesignPattern
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new Users.UserListViewModel();
+        }
+        public object CurrentViewModel { get; set; }
+    }
+}
