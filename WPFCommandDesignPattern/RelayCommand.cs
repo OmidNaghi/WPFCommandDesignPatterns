2a5d555de189e2ccf9029f08c2bec42009e1b133
OmidNaghi <43643964+OmidNaghi@users.noreply.github.com>
2018-10-01 14:50:00 +0200

    Command Pattern Developed

---
+++ WPFCommandDesignPattern/RelayCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFCommandDesignPattern
+{
+    public class RelayCommand : ICommand
+    {
+        Action _TargetExecuteMethod;
+        Func<bool> _TargetCanExecuteMethod;
+
+        public RelayCommand(Action executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public RelayCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public bool CanExecute(object parameter)
+        {
+            if (_TargetCanExecuteMethod != null) return _TargetCanExecuteMethod();
+           
+            if (_TargetExecuteMethod!= null) return true;
+           
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_TargetExecuteMethod != null) _TargetExecuteMethod();
+        }
+    }
+
+    public class RelayComman<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WPFCommandDesignPattern/Users/UserListViewModel.cs
+++ WPFCommandDesignPattern/Users/UserListViewModel.cs
@@ -18,7 +18,37 @@ namespace WPFCommandDesignPattern.Users
             if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject())) return;
             
              Users = new ObservableCollection<SecurityDataAccess.Users>(_repo.GetUsersAsync().Result);
+            DeleteCommand = new RelayCommand(OnDelete,CanDelete);
         }
+
+        private void OnDelete()
+        {
+            Users.Remove(SelectedUser);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedUser != null;
+        }
+
+        public RelayCommand DeleteCommand { get; private set; }
+
         public ObservableCollection<SecurityDataAccess.Users> Users { get; set; }
+
+        private SecurityDataAccess.Users _SelectedUser;
+
+        public SecurityDataAccess.Users SelectedUser
+        {
+            get
+            {
+                return _SelectedUser;
+            }
+            set
+            {
+                _SelectedUser = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
     }
 }
